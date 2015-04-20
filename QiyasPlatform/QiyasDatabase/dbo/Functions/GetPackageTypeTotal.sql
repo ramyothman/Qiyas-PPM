828e331dfebbaac18791f66c09cc27a83c62b3a7
rmostafa <ramy.mostafa@gmail.com>
2015-04-20 12:30:18 +0200

    Database

--- QiyasPlatform/QiyasDatabase/dbo/Functions/GetPackageTypeTotal.sql
+++ QiyasPlatform/QiyasDatabase/dbo/Functions/GetPackageTypeTotal.sql
@@ -3,20 +3,19 @@
 -- Create date: <Create Date, ,>
 -- Description:	<Description, ,>
 -- =============================================
-CREATE FUNCTION [dbo].[GetTotalPrintingPackageByPrintingID]
+CREATE FUNCTION GetPackageTypeTotal 
 (
 	-- Add the parameters for the function here
-	@ID int
+	 @PackageTypeID int
 )
 RETURNS int
 AS
 BEGIN
 	-- Declare the return variable here
 	DECLARE @Result int
-	Declare @PackageTypeID int
-	Select @PackageTypeID = PackagingTypeID from [PPM].[PackagingType] where BooksPerPackage = 3 and ExamModelCount = 1
+
 	-- Add the T-SQL statements to compute the return value here
-	SELECT @Result = SUM(PackageTotal) from [PPM].[BookPackingOperation] where [BookPrintingOperationID] = @ID and [PackagingTypeID] <> @PackageTypeID
+	SELECT @Result =  Total from [PPM].[PackagingType] as pt where pt.PackagingTypeID = @PackageTypeID
 	if(@Result is null)
 		Set @Result = 0
 	-- Return the result of the function
--- QiyasPlatform/QiyasDatabase/dbo/Functions/GetTotalPrintingPackageItemsA3ByPrintingID.sql
+++ QiyasPlatform/QiyasDatabase/dbo/Functions/GetTotalPrintingPackageItemsA3ByPrintingID.sql
@@ -3,7 +3,7 @@
 -- Create date: <Create Date, ,>
 -- Description:	<Description, ,>
 -- =============================================
-CREATE FUNCTION [dbo].[GetTotalPrintingPackageByPrintingID]
+create FUNCTION [dbo].[GetTotalPrintingPackageItemsA3ByPrintingID]
 (
 	-- Add the parameters for the function here
 	@ID int
@@ -13,10 +13,12 @@ AS
 BEGIN
 	-- Declare the return variable here
 	DECLARE @Result int
+	Declare @ExamID int
 	Declare @PackageTypeID int
 	Select @PackageTypeID = PackagingTypeID from [PPM].[PackagingType] where BooksPerPackage = 3 and ExamModelCount = 1
+	Select @ExamID = ExamID From [PPM].[BookPrintingOperation] where [BookPrintingOperationID] = @ID
 	-- Add the T-SQL statements to compute the return value here
-	SELECT @Result = SUM(PackageTotal) from [PPM].[BookPackingOperation] where [BookPrintingOperationID] = @ID and [PackagingTypeID] <> @PackageTypeID
+	SELECT @Result = SUM(PackageTotal * dbo.GetPackageTypeTotal([PackagingTypeID]) ) from [PPM].[BookPackingOperation] where [BookPrintingOperationID] = @ID and [PackagingTypeID] = @PackageTypeID and [PackingCalculationTypeID] <> 2
 	if(@Result is null)
 		Set @Result = 0
 	-- Return the result of the function
