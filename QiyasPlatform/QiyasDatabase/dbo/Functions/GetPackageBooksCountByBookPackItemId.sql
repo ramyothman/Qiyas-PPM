b8659cb94b77babd767f14fd16cb2b046dc447e2
rmostafa <ramy.mostafa@gmail.com>
2015-05-18 12:49:25 +0200

    QiyasDB Update

---
+++ QiyasPlatform/QiyasDatabase/dbo/Functions/GetPackageBooksCountByBookPackItemId.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION GetPackageBooksCountByBookPackItemId
+(
+	-- Add the parameters for the function here
+	@BookPackItemID int
+)
+RETURNS int
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @Result int
+
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @Result  = [BooksPerPackage] from [PPM].[PackagingType] 
+	where [PackagingTypeID] in (select [PackagingTypeID] from [PPM].[BookPackingOperation] 
+	where [BookPackingOperationID] in (select [BookPackingOperationID] from [PPM].[BookPackItem]
+	where [BookPackItemID] = @BookPackItemID))
+					
+
+	-- Return the result of the function
+	RETURN @Result
+
+END
---
+++ QiyasPlatform/QiyasDatabase/dbo/Functions/GetPackedBookItemLastSerial.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[GetPackedBookItemLastSerial] 
+(
+	-- Add the parameters for the function here
+	@BookPackItemID int
+)
+RETURNS int
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @Result int
+	Declare @SubBookPackItemID int
+	Declare @InitialSerial int
+
+	select @InitialSerial = [StartBookSerial] from [PPM].[BookPackItem] WHERE [BookPackItemID] = @BookPackItemID
+	if @InitialSerial is null
+		set @InitialSerial = 0
+	-- Add the T-SQL statements to compute the return value here
+	SELECT @SubBookPackItemID = Max(BookPackItemID) from [PPM].[BookPackItem] where ParentBookPackItemID = @BookPackItemID
+	select @Result = [LastBookSerial] from [PPM].[BookPackItem] where BookPackItemID = @SubBookPackItemID
+	if @Result is null
+		set @Result = @InitialSerial
+
+	-- Return the result of the function
+	RETURN @Result
+
+END
